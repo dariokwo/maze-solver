494c319e44b3671f0cfa04ee025546475a1f9354
Samuel Batali <contact@samuelbatali.com>
2021-08-02 16:10:26 -0400

    Updates to Maze class

--- main.py
+++ main.py
@@ -1,27 +1,71 @@
-from maze import create_2Dmaze
-from bfs import bfs
-from dijkstra import dijkstra
+from collections import deque
+
+from maze import IMaze
+import dijkstra
+import bfs
+import dfs
 
 
-def print_path(path) -> None:
-    for node in path:
-        print(node)
+def print_Graph_EdgeList(start_node):
+    visited = {start_node: True}
+    queue = deque([start_node])
+    
+    while len(queue) > 0:
+        node = queue.pop()
+        print("[Node{} Neighbors: [".format(node), end=" ")
+        
+        for neighbor in node.get_neighbors():
+            if neighbor not in visited:
+                queue.appendleft(neighbor)
+                visited[neighbor] = True
+            
+            print(neighbor, end=", ")
+            
+        print(']')
     
 def main():
     
-    maze_start, maze_end = create_2Dmaze("./examples/tiny.png")
+    image_source  = "./examples/tiny.png"
+    maze = IMaze(image_source)
     
-    # Path of unweighted maze
-    print("Unweighted maze path:")
-    bfs_path = bfs(maze_start, maze_end)
-    print_path(bfs_path)
+    maze2d, width, height = maze.get_maze2D()
+    print("2D Image Maze")
+    print(maze2d)
+    print()
     
-    # print()
+    maze_entrance, maze_exit = maze.get_mazeGraph()
+    print("Maze connected Graph")
+    print_Graph_EdgeList(maze_entrance)
+    print()
     
-    # Path of unweighted maze
-    # print("Weighted maze path (Shortest path):")
-    # shortest_path = dijkstra(maze_start, maze_end, True)
-    # print_path(shortest_path)
+    print("Entrance vertex: ", maze_entrance)
+    print("Exit Vertex: ", maze_exit)
+    print()
+    
+    path, traversal_order  = bfs.bfs(maze_entrance, maze_exit, order=True)
+    print("Find Path from Entrance to Exit using Bread First Search")
+    print("Path", path)
+    print("Traversal Order", traversal_order)
+    print()
+    
+    path, traversal_order  = dfs.dfs(maze_entrance, maze_exit, order=True)
+    print("Find Path from Entrance to Exit using Depth First Search")
+    print("Path", path)
+    print("Traversal Order", traversal_order)
+    print()
+    
+    path, traversal_order  = dfs.dfs_recursive(maze_entrance, maze_exit, order=True)
+    print("Find Path from Entrance to Exit using Recursive Depth First Search")
+    print("Path", path)
+    print("Traversal Order", traversal_order)
+    print()
+
+    print("Find Shortest Path and distance from Entrance to Exit using Dijkstra's Algorithm")
+    path, total_distance, traversal_order = dijkstra.dijkstra(maze_entrance, maze_exit, order=True)
+    print("Total distance: ", total_distance)
+    print("Path: ", path)
+    print("Traversal order: ", traversal_order)
+    
 
 if __name__ == "__main__":
     main()
--- maze.py
+++ maze.py
@@ -2,35 +2,37 @@ from PIL import Image
 import numpy as np
 
 from node import Node
-from dfs import dfs_recursive
 
 class IMaze:
     def __init__(self, image_source = None) -> None:
         self.Image_source = image_source
-        self.maze_2d = None
+        
+        self.maze_2d = np.array([])
         self.height = 0
         self.width = 0
+        
+        self.image = None
 
         self.entrance = None
         self.exit = None
 
         if image_source != None:
-            self.get_2Dmaze()
+            self.get_maze2D(image_source)
 
     def get_maze2D(self, imgSrc = None):
         """REturn 2D maze of a given image"""
 
         # If image was provided in constructor
-        if imgSrc == None and self.maze_2d != None:
+        if imgSrc == None and np.size(self.maze_2d) > 0:
             return self.maze_2d, self.width, self.height
 
         # Make sure image is provided
         assert imgSrc != None
 
         # Load and get image data
-        img = Image.open(imgSrc)
-        width, height = img.width, img.height
-        maze = np.reshape(list(img.getdata(0)), (width, height))
+        self.image = Image.open(imgSrc)
+        width, height = self.image.width, self.image.height
+        maze = np.reshape(list(self.image.getdata(0)), (width, height))
 
         # Save data in class variables
         self.maze_2d = maze
@@ -41,15 +43,94 @@ class IMaze:
         return maze, width, height
 
     def get_mazeGraph(self, maze = None, width = None, height = None):
+
+        # It it has already been calculated
+        if maze is None and self.entrance is not None and self.exit is not None:
+            return self.entrance, self.exit
+
+        def get_entrance(maze, width):
+            for col in range(1, width):
+                if maze[0][col] == 1:
+                    return Node((0, col)), col
+            raise Exception("Maze Error: Maze have no entrance on top row")
+
+        def get_exit(maze, width, height):
+            for col in range(1, width):
+                if maze[height - 1][col] == 1:
+                    return Node((height - 1, col)), col
+            raise Exception("Maze Error: Maze have no exit on bottom row")
         
-        assert maze or self.maze_2d
+        assert maze is not None or self.maze_2d is not None
         if(maze == None):
             maze = self.maze_2d
             width = self.width
             height = self.height
 
-        start_node = end_node = None
         top_row = [None] * width
+        self.entrance, col = get_entrance(maze, width)
+        top_row[col] = self.entrance
+        
+        for row in range(1, height - 1):
+            left_node = None
+            for col in range(1, width - 1):
+                
+                # Do nothing on a wall
+                if maze[row][col] == 0: continue
+                new_node = None
+                
+                # prev = 1 (left node must exit)
+                if maze[row][col - 1] == 1:
+                    assert left_node is not None
+
+                    # next = 1
+                    if maze[row][col+1] == 1:
+                        # top = 1 or bottom = 1
+                        if maze[row-1][col] == 1 or maze[row+1][col] == 1:
+                            new_node = Node((row, col))
+                            new_node.add_neighbor(left_node)
+                            left_node.add_neighbor(new_node)
+                            left_node = new_node
+                        # else do nothing (meaning prev = 1, next = 1, top = 0, and bottom = 0)
+                    
+                    # next = 0 (Must be end of corridor)
+                    else:
+                        new_node = Node((row, col))
+                        new_node.add_neighbor(left_node)
+                        left_node.add_neighbor(new_node)
+                        left_node = None
+                
+                # prev = 0 (No left node)
+                else: 
+                    # next = 1 (Must be start of a corridor)
+                    if maze[row][col+1] == 1:
+                        new_node = Node((row, col))
+                        left_node = new_node
+                    
+                    # next = 0
+                    else:
+                        # top = 0 or bottom = 0 (Must be a dead end)
+                        if maze[row-1][col] == 0 or maze[row+1][col] == 0:
+                            new_node = Node((row, col))
+                            
+                        # else do nothing (prev = 0, next = 0, top = 1, bottom = 1)
+
+                if new_node is not None:
+                    # top col = 1 (Node exist above)
+                    if maze[row - 1][col] == 1 and top_row[col] != None:
+                        new_node.add_neighbor(top_row[col])
+                        top_row[col].add_neighbor(new_node)
+
+                    # Add new node to top_row if needed
+                    top_row[col] = new_node if maze[row+1][col] else None
+        
+        self.exit, col = get_exit(maze, width, height)
+        if(top_row[col] is None): 
+            raise Exception("Maze Error: no path to exit")
+
+        self.exit.add_neighbor(top_row[col])
+        top_row[col].add_neighbor(self.exit)
+        
+        return self.entrance, self.exit
 
 
 
@@ -164,10 +245,19 @@ def create_2Dmaze(input_image):
             
 
 if __name__ == "__main__":
-    # maze_start, maze_end = create_2Dmaze("./examples/tiny.png")
-    # dfs_recursive(maze_start, maze_end, True)
-    # print(maze_start) 
 
     image_source  = "./examples/tiny.png"
-    maze = Image(image_source)
+    maze = IMaze(image_source)
+    
+    maze2d, width, height = maze.get_maze2D()
+    print("2D Image Maze")
+    print(maze2d)
+    print()
+    
+    maze_entrance, maze_exit = maze.get_mazeGraph()
+    print("Maze connected Graph")
+    print("Entrance vertex: ", maze_entrance)
+    print("Exit Vertex: ", maze_exit)
+    
+    
     
